cb3f002855cd1a5b17244cf3da00f79709048dc3
Kim Zülsdorff <kim.zuelsdorff@hotmail.de>
2017-05-04 18:24:10 +0200

    2. Entity hinzugefügt (New Game-Button)

--- src/index.js
+++ src/index.js
@@ -8,6 +8,16 @@ const game = new ECS();
 const menu = new ECS();
 let current_state = menu;
 
+const Container = PIXI.Container,
+    autoDetectRenderer = PIXI.autoDetectRenderer,
+    loader = new PIXI.loaders.Loader(),
+    resources = PIXI.loader.resources,
+    TextureCache = PIXI.utils.TextureCache,
+    Texture = PIXI.Texture,
+    Graphics = PIXI.Graphics,
+    Text = PIXI.Text;
+    //Sprite = PIXI.Sprite;
+
 function gameLoop() {
     current_state.update();
     requestAnimationFrame(gameLoop);
@@ -17,14 +27,21 @@ function startGame () {
     menu.addSystem(new Render());
 
     let entity = new ECS.Entity(null, [Sprite]);
-    console.log(entity);
-    entity.components.sprite.pixiSprite = new PIXI.Sprite.fromImage('red_square');
+		let redSquare = entity.components.sprite;
+    redSquare.pixiSprite = new PIXI.Sprite.fromImage('red_square');
+		redSquare.pixiSprite.position.set(100, 0);
     menu.addEntity(entity);
+    let entity2 = new ECS.Entity(null, [Sprite]);
+		let newGame = entity2.components.sprite;
+		newGame.pixiSprite = new Text("New Game",
+													{fontFamily: "Arial", fontSize: 32, fill: "blue"});
+		newGame.pixiSprite.position.set(100, 100);
+    newGame.interactive = true;
+		menu.addEntity(entity2);
+		
 
     gameLoop();
 }
-
-const loader = new PIXI.loaders.Loader();
 
 loader
     .add('red_square', '/img/red_square.png')
