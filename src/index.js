a43df92526a04a0196294662604cfbcb90cf038c
Hans Ole Hatzel <hhatzel@gmail.com>
2017-05-11 18:12:46 +0200

    Performance experiments

--- src/index.js
+++ src/index.js
@@ -1,67 +1,92 @@
+import Sprite from './components/Sprite.js';
+import Render from './systems/Render.js';
 import * as PIXI from 'pixi.js';
 import ECS from 'yagl-ecs';
+let renderer = PIXI.autoDetectRenderer(1920, 1080, {
+  resolution: window.devicePixelRatio || 1
+});
+renderer.backgroundColor = 0xFFFFFF;
 
-import Render from './systems/Render.js';
-import Movement from './systems/Movement.js';
-import Sprite from './components/Sprite.js';
-import ButtonSystem from 'systems/Button';
-import Button from 'components/Button';
-import createGameEntities from 'createGameEntities';
-import globals from 'globals';
+document.body.appendChild(renderer.view);
+document.body.style.margin = '0';
+
+var container = new PIXI.Container();
+let stage = new PIXI.Container();
 
 const game = new ECS();
-const menu = new ECS();
 let current_state = game;
-let ticker;
 
 function gameLoop () {
   window.dt = ticker.deltaTime;
-  current_state.update();
+  // current_state.update();
+  renderer.render(stage);
+}
+container.addChild(stage);
+let ticker = new PIXI.ticker.Ticker();
+game.addSystem(new Render(renderer, 1920, 1080, stage));
+ticker.add(gameLoop);
+ticker.start();
+
+var texture = PIXI.Texture.fromImage('/img/slot.png');
+
+// Create a 5x5 grid of bunnies
+for (var i = 0; i < 256; i++) {
+  let entity = new ECS.Entity(null, [Sprite]);
+  var bunny = new PIXI.Sprite(texture);
+  bunny.anchor.set(0.5);
+  bunny.x = (i % 16) * 40;
+  bunny.y = Math.floor(i / 16) * 40;
+  stage.addChild(bunny);
+  entity.components.sprite.pixiSprite = bunny;
+  game.addEntity(entity);
 }
 
-const loader = new PIXI.loaders.Loader();
 
-function startGame () {
-  let renderer = PIXI.autoDetectRenderer(globals.width, globals.height, {
-    resolution: window.devicePixelRatio || 1
-  });
-  renderer.backgroundColor = 0xFFFFFF;
-  // renderer.view.style.border = '1px solid black'
-  document.body.appendChild(renderer.view);
-  document.body.style.margin = '0';
+// Center on the screen
+container.x = (renderer.width - container.width) / 2;
+container.y = (renderer.height - container.height) / 2;
 
-  menu.addSystem(new Render(renderer, globals.width, globals.height));
-  menu.addSystem(new ButtonSystem());
+// import Movement from './systems/Movement.js';
+// import ButtonSystem from 'systems/Button';
+// import Button from 'components/Button';
+// import createGameEntities from 'createGameEntities';
+// import globals from 'globals';
 
-  game.addSystem(new Render(renderer, globals.width, globals.height));
-  game.addSystem(new Movement());
+// let ticker;
 
-  // Create menu entities
-  let entity = new ECS.Entity(null, [Sprite, Button]);
-  let redSquare = entity.components.sprite;
-  redSquare.pixiSprite = new PIXI.Sprite.fromImage('red_square');
-  redSquare.pixiSprite.position.set(100, 0);
-  menu.addEntity(entity);
 
-  let entity2 = new ECS.Entity(null, [Sprite]);
-  let newGame = entity2.components.sprite;
-  newGame.pixiSprite = new PIXI.Text('New Game',
-    {fontFamily: 'Arial', fontSize: 32, fill: 'blue'});
-  newGame.pixiSprite.position.set(100, 100);
-  newGame.interactive = true;
-  menu.addEntity(entity2);
+// const loader = new PIXI.loaders.Loader();
 
-  createGameEntities().forEach(e => game.addEntity(e));
+// function startGame () {
 
-  ticker = new PIXI.ticker.Ticker();
-  ticker.add(gameLoop);
-  ticker.start();
-}
+  // menu.addSystem(new ButtonSystem());
 
-loader
-  .add('red_square', '/img/red_square.png')
-  .add('tower_weak', '/img/tower_weak.png')
-  .add('tower_strong', '/img/tower_strong.png')
-  .add('tower_long', '/img/tower_long.png')
-  .add('slot', '/img/slot.png')
-  .load(startGame);
+  // game.addSystem(new Render(renderer, globals.width, globals.height));
+  // game.addSystem(new Movement());
+
+  // // Create menu entities
+  // let entity = new ECS.Entity(null, [Sprite, Button]);
+  // let redSquare = entity.components.sprite;
+  // redSquare.pixiSprite = new PIXI.Sprite.fromImage('red_square');
+  // redSquare.pixiSprite.position.set(100, 0);
+  // menu.addEntity(entity);
+
+  // let entity2 = new ECS.Entity(null, [Sprite]);
+  // let newGame = entity2.components.sprite;
+  // newGame.pixiSprite = new PIXI.Text('New Game',
+  //   {fontFamily: 'Arial', fontSize: 32, fill: 'blue'});
+  // newGame.pixiSprite.position.set(100, 100);
+  // newGame.interactive = true;
+  // menu.addEntity(entity2);
+
+//   createGameEntities().forEach(e => game.addEntity(e));
+
+// }
+
+// loader
+//   .add('red_square', '/img/red_square.png')
+//   .add('tower_weak', '/img/tower_weak.png')
+//   .add('tower_strong', '/img/tower_strong.png')
+//   .add('tower_long', '/img/tower_long.png')
+//   .add('slot', '/img/slot.png')
+//   .load(startGame);
--- src/systems/Render.js
+++ src/systems/Render.js
@@ -2,16 +2,14 @@ import * as PIXI from 'pixi.js';
 import ECS from 'yagl-ecs';
 
 export default class Render extends ECS.System {
-  constructor (renderer, width, height) {
+  constructor (renderer, width, height, stage) {
     super();
 
     this.renderer = renderer;
     this.width = width;
     this.height = height;
-    this.stage = new PIXI.Container();
+    this.stage = stage;
 
-    window.addEventListener('resize', this.resizeHandler.bind(this), false);
-    this.resizeHandler();
   }
 
   test (entity) {
@@ -29,25 +27,6 @@ export default class Render extends ECS.System {
 
   update (entity) {
     this.renderer.render(this.stage);
-  }
-
-  resizeHandler () {
-    console.log('innerWidth', window.innerWidth);
-    console.log('innerHeight', window.innerHeight);
-
-    const scaleFactor = Math.min(
-      window.innerWidth / this.width,
-      window.innerHeight / this.height
-    );
-
-    const newWidth = Math.ceil(this.width * scaleFactor);
-    const newHeight = Math.ceil(this.height * scaleFactor);
-
-    this.renderer.view.style.width = `${newWidth}px`;
-    this.renderer.view.style.height = `${newHeight}px`;
-
-    this.renderer.resize(newWidth, newHeight);
-    this.stage.scale.set(scaleFactor);
   }
 
 }
