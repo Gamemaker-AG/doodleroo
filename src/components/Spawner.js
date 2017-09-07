b4abcaf8b62dc75ea4b2b923d9bc2ee090af5fc1
Rafael Epplee <raffomania@gmail.com>
2017-09-07 16:44:44 +0200

    Fix indentation

--- src/components/Spawner.js
+++ src/components/Spawner.js
@@ -1,4 +1,3 @@
-import Vector from 'vigur';
 import PixiVector from 'PixiVector';
 import globals from 'globals';
 
@@ -11,18 +10,18 @@ export default {
     enemyImageName: 'tower_strong',
     enemyComponents: () => {
       return {
-      'enemy': {},
-      'gridPosition': {x: 0, y: 0},
-      'movement': {
-        velocity: new PixiVector(0, 0),
-        angularVelocity: 0,
-        maxSpeed: 100
-      },
-      'goal': {x: Math.floor(slotCount / 2), y: slotCount - 1},
-      'autoUpdateGridPosition': {},
-      'followPath': {},
-      'health': {health: 50}
-      }
+        'enemy': {},
+        'gridPosition': {x: 0, y: 0},
+        'movement': {
+          velocity: new PixiVector(0, 0),
+          angularVelocity: 0,
+          maxSpeed: 100
+        },
+        'goal': {x: Math.floor(slotCount / 2), y: slotCount - 1},
+        'autoUpdateGridPosition': {},
+        'followPath': {},
+        'health': {health: 50}
+      };
     },
     count: 0,
     interval: 1
