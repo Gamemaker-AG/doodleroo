018767960c0b96f787cac481a40b3ae0274379c3
Rafael Epplee <raffomania@gmail.com>
2017-09-07 16:51:59 +0200

    add source maps

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'cheap-module-eval-source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
@@ -10,6 +11,6 @@ module.exports = {
     contentBase: path.join(__dirname, 'assets')
   },
   resolve: {
-  	modules: [path.resolve(__dirname, 'src'), 'node_modules']
+    modules: [path.resolve(__dirname, 'src'), 'node_modules']
   }
 };
