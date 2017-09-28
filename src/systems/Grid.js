af1d84d47e19cbed3a54af58da297e4924cf793e
Hans Ole Hatzel <hhatzel@gmail.com>
2017-09-28 15:29:15 +0200

    Fix off by one error that caused crashes when building slow tower.

--- src/systems/Grid.js
+++ src/systems/Grid.js
@@ -63,8 +63,8 @@ export default class Grid extends ECS.System {
     let { gridPosition } = entity.components;
     let x_lower = Math.floor(Math.max(gridPosition.x - range, 0));
     let y_lower = Math.floor(Math.max(gridPosition.y - range, 0));
-    let x_upper = Math.ceil(Math.min(gridPosition.x + range, globals.slotCount));
-    let y_upper = Math.ceil(Math.min(gridPosition.y + range, globals.slotCount));
+    let x_upper = Math.ceil(Math.min(gridPosition.x + range, globals.slotCount - 1));
+    let y_upper = Math.ceil(Math.min(gridPosition.y + range, globals.slotCount - 1));
     for (let x = x_lower; x <= x_upper; x++) {
       for (let y = y_lower; y <= y_upper; y++) {
         let slot_pos = new PixiVector(x, y);
