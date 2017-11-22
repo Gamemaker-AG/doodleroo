28f9d52c483e92414b29fda34a46b0a4dea63d8c
fabianbehrendt <fabian.behrendt95@gmail.com>
2017-11-22 16:47:01 +0100

    add random function to math-utils

--- src/math-utils.js
+++ src/math-utils.js
@@ -14,3 +14,25 @@ export function radianLerp (start, end, percent) {
 
   return lerp(start, newEnd, percent);
 };
+
+export function random (min, max) {
+  var rand = Math.random();
+
+  if (typeof min === 'undefined') {
+    return rand;
+  } else if (typeof max === 'undefined') {
+    if (min instanceof Array) {
+      return min[Math.floor(rand * min.length)];
+    } else {
+      return rand * min;
+    }
+  } else {
+    if (min > max) {
+      var tmp = min;
+      min = max;
+      max = tmp;
+    }
+
+    return rand * (max - min) + min;
+  }
+};
